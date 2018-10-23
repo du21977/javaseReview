555bd15e25087dd8177c99533c0e602369db6ec8
dobi <549914283@qq.com>
2018-10-23 11:58:02 +0800

    添加数据结构--链表

--- data_structure/src/com/dobi/day3_Stacks_and_Queues/Day3_8.java
+++ data_structure/src/com/dobi/day3_Stacks_and_Queues/Day3_8.java
@@ -1,11 +1,43 @@
 package com.dobi.day3_Stacks_and_Queues;
 
+import java.util.Random;
+
 /**
  * 数组队列和循环队列的比较
+ * ArrayQueue time :3.771243042s
+ * LoopQueue time :0.018807194s
  */
 public class Day3_8 {
 
     public static void main(String[] args) {
 
+        int opCount =100000;
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time = testQueue(arrayQueue,opCount);
+        System.out.println("ArrayQueue time :"+time+"s");
+
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time1 = testQueue(loopQueue,opCount);
+        System.out.println("LoopQueue time :"+time1+"s");
+
+    }
+
+    //测试入队和出队需要的时间s
+    private static double testQueue(Queue<Integer>queue,int opCount){
+        long startTime = System.nanoTime(); //单位是us纳秒
+
+        Random random = new Random();
+        for (int i=0;i<opCount;i++){
+            queue.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+
+        for (int i=0;i<opCount;i++){
+           queue.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime)/1000000000.0;
     }
 }
---
+++ data_structure/src/com/dobi/day4_Linked_List/day4_1.java
@@ -0,0 +1,20 @@
+package com.dobi.day4_Linked_List;
+
+/**
+ * 什么是链表
+ */
+public class day4_1 {
+
+    //前面学过的线性数据结构：动态数组，栈，队列
+    //底层依赖的是静态数组，靠resize解决固定容量问题
+
+
+    //链表---真正的动态数据结构---最简单的动态数据结构
+    //数据存储在节点Node中
+    /*
+    class Node{
+        E e;
+        Node next;
+    }*/
+
+}
